b28bd1f5b871d2b78f284a503685088d45739e99
parkminwoo <parkminwoo1991@gmail.com>
2023-03-02 14:15:55 +0900

    feat: update config

---
+++ config.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+class Config:
+    parser = argparse.ArgumentParser(description="for gluon weight inference only")
+    parser.add_argument('--gluon_weight_path', default="./weights/ko-news-clf-gluon-weight.pth")
+    parser.add_argument('--data_path', default="./data/sample.csv")
+    parser.add_argument('--save_path', default=None)
+    parse = parser.parse_args()
+    params = {
+        'GLUON_WEIGHT_PATH': parse.gluon_weight_path,
+        'SAVE_PATH': parse.save_path,
+        'DATA_PATH': parse.data_path,
+    }
+    gluon_cfg = {'max_len': 128, 'batch_size':32}
