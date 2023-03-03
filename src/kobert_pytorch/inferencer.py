3252f2c7ff8d31b5e8646748d944addd89e38258
parkminwoo <parkminwoo1991@gmail.com>
2023-03-03 16:53:22 +0900

    feat: single text inference

--- src/kobert_pytorch/inferencer.py
+++ src/kobert_pytorch/inferencer.py
@@ -1,6 +1,14 @@
+#-*- coding: utf-8 -*-
+
 import numpy as np
 import pandas as pd
 import torch
+import sys
+from pathlib import Path
+file = Path(__file__).resolve()
+package_root_directory = file.parents[1]
+sys.path.append(str(package_root_directory))
+
 import gluonnlp as nlp
 from tqdm.notebook import tqdm
 from kobert.pytorch_kobert import get_tokenizer
@@ -9,6 +17,7 @@ from transformers import AdamW
 from dataloader import BERTDataset
 from preprocess.processor import NLPdata
 from model import BERTClassifier
+
 
 max_len = 64
 batch_size = 64
@@ -38,27 +47,32 @@ def torch_bert_inference(model: object, dataloader: object, save_path: str, devi
     if save_path is not None:
         df_eval.to_csv(save_path, encoding='utf-8-sig', index=False)
     else:
-        print(class_dict[predicted_y[0]])
+        print(f'>>> Prdicted news topic: {class_dict[predicted_y[0]]}')
         
     return df_eval
 
 # TEMPORAL METHOD
-# def single_text_inference(self, input_text):
-#     dataset_inf = [[input_text, '99']]
-#     data_inf = BERTDataset(dataset_inf, 0, 1, self.tok, self.cfg['max_len'], True, False)
-#     inf_dataloader = torch.utils.data.DataLoader(data_inf, batch_size=self.cfg['batch_size'])
-#     for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(inf_dataloader):
-#         token_ids = token_ids.long().to(self.device)
-#         segment_ids = segment_ids.long().to(self.device)
-#         valid_length= valid_length
-#         out = self.model(token_ids, valid_length, segment_ids)
-#         cls_pooled_denselayer = out.detach().numpy()
-#         predicted_y = np.argmax(cls_pooled_denselayer)
-        
-#     return cls_pooled_denselayer,  predicted_y
+def single_text_inference(model: object, cfg: dict, input_text: str):
+    class_dict = {0: 'international', 1: 'economy', 2: 'society', 3: 'sport', 4: 'it', 5: 'politics', 6: 'entertain', 7: 'culture'}
+    dataset_inf = [[input_text, '99']]
+    data_inf = BERTDataset(dataset_inf, 0, 1, tok, cfg['max_len'], True, False)
+    inf_dataloader = torch.utils.data.DataLoader(data_inf, batch_size=cfg['batch_size'])
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(inf_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        out = model(token_ids, valid_length, segment_ids)
+        cls_pooled_denselayer = out.detach().numpy()
+        predicted_y = np.argmax(cls_pooled_denselayer)
+    
+    print(f'>>> Prdicted news topic: {class_dict[predicted_y]}')
+
+    return cls_pooled_denselayer,  predicted_y
 
 
 if __name__ == "__main__":
+    cfg = {'max_len' : 64,
+           'batch_size' : 64}
     tokenizer = get_tokenizer()
     bertmodel, vocab = get_pytorch_kobert_model(cachedir=".cache")
     tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
@@ -79,4 +93,7 @@ if __name__ == "__main__":
     model.load_state_dict(checkpoint['model_state_dict'])
     optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
-    torch_bert_inference(model, test_dataloader, None, device)
+    # torch_bert_inference(model, test_dataloader, None, device)
+
+    sample_input = '한양대 경제학부 하준경 교수는 BBC 코리아에 현재 50,60대에 해당하는 기성세대가 1990년대 집을 살 때는 빚을 많이 지지 않고도 연소득의 서너 배, 혹은 다섯 배 정도면 충분히 괜찮은 집을 살 수 있었지만 현재는 소득 대비 집값이 서울 같은 경우 20배씩 되는 정도기 때문에 빚을 많이 질 수 밖에 없다며 특히 청년 세대의 경우 과거에 이미 집을 싸게 사 집값 상승의 효과를 누렸던 기성세대와 달리 집을 사기 위해 빚을 많이 내야 하기 때문에 부채 증가가 빠르게 되는 부분이 있다고 설명했다 이에 대해 수원에 거주하는 61세 A씨는 1980~90년대에는 10년만 열심히 하면 집을 살 수 있다는 생각을 너도 나도 했다며 실제 같은 또래 대부분의 주변 사람들이 그렇게 약간의 빚을 내 집을 구했다고 말했다 실제 위 보고서의 분석에 따르면 전체 가구에 비해 청년 가구의 전체 자산 규모 확대 속도가 떨어지고, 전체 자산 중 실물자산이 차지하는 비중도 작은 경향을 확인할 수 있다'
+    single_text_inference(model, cfg, sample_input)
